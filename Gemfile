c8add918ef0ca7661ad27872163b691010d179e2
Joe Teichman <kaalm1@gmail.com>
2017-05-07 17:20:07 +0000

    Finishing Task 6

--- Gemfile
+++ Gemfile
@@ -1,6 +1,8 @@
 source 'https://rubygems.org'
 
 
+gem 'twilio-ruby'
+gem 'figaro'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.0.0'
 # Use sqlite3 as the database for Active Record
@@ -26,7 +28,6 @@ gem 'jbuilder', '~> 2.5'
 # gem 'redis', '~> 3.0'
 # Use ActiveModel has_secure_password
 gem 'bcrypt', '~> 3.1.7'
-gem 'twilio-ruby', '~4.13.0'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,16 @@ class PagesController < ApplicationController
   end
 
   def sms
-    
+     require 'twilio-ruby'
+
+      account_sid = ENV["twilio_account_id"] # Your Account SID from www.twilio.com/console
+      auth_token = ENV["twilio_token"]   # Your Auth Token from www.twilio.com/console
+
+      @client = Twilio::REST::Client.new account_sid, auth_token
+      message = @client.account.messages.create(:body => params[:message],
+    :to => params[:number],    # Replace with your phone number
+    :from => "+13234258562")  # Replace with your Twilio number
+
+    puts message.sid
   end
 end
